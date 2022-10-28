e1530bdf30d6e5c755c9066d91244f141794702e
Alex Brandan <alex.brandan@sarsy-srl.com>
2022-10-27 21:55:28 -0300

    Se modifica registro empresa y se añade el registro usuario

---
+++ app/src/main/java/com/example/inclujobs/registro_empresa.java
@@ -0,0 +1,14 @@
+package com.example.inclujobs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registro_empresa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_empresa);
+    }
+}
