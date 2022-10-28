bd817888a57457274399ac456238815a818df288
Alex Brandan <alex.brandan@sarsy-srl.com>
2022-10-28 15:19:08 -0300

    Se añade la redirección de los botones iniciar sesion, registro empresa y registro usuario a las pantallas correspondientes. se modifica estilo.

--- app/src/main/java/com/example/inclujobs/MainActivity.java
+++ app/src/main/java/com/example/inclujobs/MainActivity.java
@@ -2,13 +2,59 @@ package com.example.inclujobs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnRegistroEmpresa;
+    private Button btnRegistroUsuario;
+    private Button btnIniciarSesion;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnRegistroEmpresa = (Button) findViewById(R.id.btnRegistroEmpresa);
+        btnRegistroEmpresa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openRegistroEmpresa();
+            }
+        });
+
+        btnRegistroUsuario = (Button) findViewById(R.id.btnRegistrarse);
+        btnRegistroUsuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openRegistroUsuario();
+            }
+        });
+
+        btnIniciarSesion = (Button) findViewById(R.id.btnIniciarSesion);
+        btnIniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openIniciarSesion();
+            }
+        });
+    }
+
+    private void openRegistroEmpresa(){
+        Intent intent = new Intent(this, registro_empresa.class);
+        startActivity(intent);
+    }
+
+    private void openRegistroUsuario(){
+        Intent intent = new Intent(this, registro_usuario.class);
+        startActivity(intent);
+    }
+
+    private void openIniciarSesion(){
+        Intent intent = new Intent(this, login.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/inclujobs/login.java
+++ app/src/main/java/com/example/inclujobs/login.java
@@ -2,13 +2,44 @@ package com.example.inclujobs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class login extends AppCompatActivity {
+    private Button btnRegistroEmpresa;
+    private Button btnRegistroUsuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnRegistroEmpresa = (Button) findViewById(R.id.btnRegistroEmpresaLogin);
+        btnRegistroEmpresa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openRegistroEmpresa();
+            }
+        });
+
+        btnRegistroUsuario = (Button) findViewById(R.id.btnRegistrarseLogin);
+        btnRegistroUsuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openRegistroUsuario();
+            }
+        });
+    }
+
+    private void openRegistroEmpresa(){
+        Intent intent = new Intent(this, registro_empresa.class);
+        startActivity(intent);
+    }
+
+    private void openRegistroUsuario(){
+        Intent intent = new Intent(this, registro_usuario.class);
+        startActivity(intent);
     }
 }
