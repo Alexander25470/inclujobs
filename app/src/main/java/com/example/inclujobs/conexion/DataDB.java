62d652f2498796c5a6eee501961ade64e30b9872
Alexander <alexanderpopp99@gmail.com>
2022-10-28 19:12:28 -0300

    credenciales bbdd

--- app/src/main/java/com/example/inclujobs/conexion/DataDB.java
+++ app/src/main/java/com/example/inclujobs/conexion/DataDB.java
@@ -4,9 +4,9 @@ public class DataDB {
     //Información de la BD
     public static String host="sql10.freesqldatabase.com";
     public static String port="3306";
-    public static String nameBD="sql10524962";
-    public static String user="sql10524962";
-    public static String pass="JVDE1NWqjy";
+    public static String nameBD="sql10529809";
+    public static String user="sql10529809";
+    public static String pass="bzqCMBbks2";
 
     //Información para la conexion
     public static String urlMySQL = "jdbc:mysql://" + host + ":" + port + "/"+nameBD;
