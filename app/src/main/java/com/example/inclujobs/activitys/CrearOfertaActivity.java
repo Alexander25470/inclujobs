e163469f7311b1dd010fc0e8cf9363d803e07b84
ZairMar <Zair.Mar@Sarsy-srl.com>
2022-11-06 19:46:15 -0300

    crear oferta, dataInsert, view

--- app/src/main/java/com/example/inclujobs/activitys/CrearOfertaActivity.java
+++ app/src/main/java/com/example/inclujobs/activitys/CrearOfertaActivity.java
@@ -1,16 +1,68 @@
 package com.example.inclujobs.activitys;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
 import com.example.inclujobs.R;
+import com.example.inclujobs.conexion.DataInsertOferta;
+import com.example.inclujobs.conexion.DataUpdateOferta;
+import com.example.inclujobs.entidades.Oferta;
+import com.example.inclujobs.helpers.ICallBack;
+import com.google.gson.Gson;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class CrearOfertaActivity extends AppCompatActivity {
+    private Oferta oferta;
+    private TextView tvTituloCrear, tvDescripcionCrear, tvSalarioCrear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_oferta);
+        oferta = new Oferta();
+
+        tvTituloCrear = findViewById(R.id.tvTituloCrear);
+        tvDescripcionCrear = findViewById(R.id.tvDescripcionCrear);
+        tvSalarioCrear = findViewById(R.id.tvSalarioCrear);
+
     }
+
+    public void crearOferta(View view){
+
+        if( tvTituloCrear.getText().toString() == null || tvTituloCrear.getText().toString().isEmpty()){
+            Toast toast = Toast.makeText(getApplicationContext(),"Debe ingresar un Titulo", Toast.LENGTH_SHORT);
+            toast.show();
+            return;
+        }
+
+        if( tvDescripcionCrear.getText().toString() == null || tvDescripcionCrear.getText().toString().isEmpty()){
+            Toast toast = Toast.makeText(getApplicationContext(),"Debe ingresar una Descripcion", Toast.LENGTH_SHORT);
+            toast.show();
+            return;
+        }
+
+        if( tvSalarioCrear.getText().toString() == null || tvSalarioCrear.getText().toString().isEmpty()){
+            Toast toast = Toast.makeText(getApplicationContext(),"Debe ingresar un Salario", Toast.LENGTH_SHORT);
+            toast.show();
+            return;
+        }
+
+        oferta.setTitulo(tvTituloCrear.getText().toString());
+        oferta.setDescripcion(tvDescripcionCrear.getText().toString());
+        oferta.setSalario((Float.parseFloat(tvSalarioCrear.getText().toString())));
+
+        DataInsertOferta task = new DataInsertOferta(oferta, new ICallBack() {
+            @Override
+            public void function(Object obj) {
+                Toast toast = Toast.makeText(getApplicationContext(),"Se creo la oferta correctamente", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+        task.execute();
+
+    }
+
 }
--- app/src/main/java/com/example/inclujobs/conexion/DataInsertOferta.java
+++ app/src/main/java/com/example/inclujobs/conexion/DataInsertOferta.java
@@ -1,4 +1,56 @@
 package com.example.inclujobs.conexion;
 
-public class DataInsertOferta {
+import android.os.AsyncTask;
+
+import com.example.inclujobs.DTOs.CVDTO;
+import com.example.inclujobs.entidades.Oferta;
+import com.example.inclujobs.helpers.ICallBack;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class DataInsertOferta extends AsyncTask<String, Void, String> {
+
+    private Oferta oferta;
+    private ICallBack callBack;
+
+    private int result;
+    private static String result2;
+
+    public DataInsertOferta(Oferta oferta, ICallBack callBack){
+        this.oferta = oferta;
+        this.callBack = callBack;
+    }
+
+    protected String doInBackground(String... urls) {
+        String response = "";
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection(DataDB.urlMySQL, DataDB.user, DataDB.pass);
+            String query = "INSERT INTO `Ofertas`(`idEmpresa`, `Titulo`, `Descripcion`, `Salario`) VALUES (?,?,?,?);";
+            PreparedStatement ps = con.prepareStatement(query);
+
+            ps.setInt(1, oferta.getEmpresa().getId());
+            ps.setString(2,  oferta.getTitulo());
+            ps.setString(3, oferta.getDescripcion());
+            ps.setFloat(4, oferta.getSalario());
+
+            result = ps.executeUpdate();
+
+            response = "Conexion exitosa";
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+            result2 = "Conexion no exitosa";
+        }
+        return response;
+    }
+
+    @Override
+    protected void onPostExecute(String response) {
+        callBack.function(result2);
+    }
+
 }
