0b97b93357e22370fa73fc22a6c307abe38d89a4
Alexander <alexanderpopp99@gmail.com>
2022-11-06 14:37:09 -0300

    toolbar listado empresas

--- app/src/main/java/com/example/inclujobs/activitys/ListadoEmpresasActivity.java
+++ app/src/main/java/com/example/inclujobs/activitys/ListadoEmpresasActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.inclujobs.R;
 import com.example.inclujobs.conexion.DataListadoEmpresas;
 import com.example.inclujobs.entidades.Empresa;
+import com.example.inclujobs.entidades.Usuario;
 import com.example.inclujobs.helpers.ICallBack;
+import com.example.inclujobs.helpers.UserHelper;
 import com.google.gson.Gson;
 
 import android.content.Context;
@@ -14,22 +16,34 @@ import android.view.View;
 import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class ListadoEmpresasActivity extends AppCompatActivity {
     private ListView lvEmpresas;
     private ArrayList<Empresa> listaEmpresas = new ArrayList<Empresa>();
+    private TextView tvUsuarioTB; // ToolBar Listado Ofertas
+    private Button btnPublicarOfertaTB;
+    private Usuario user;
+    private final int REQUEST_LOGIN = 1;
+    private final int REQUEST_PUBLICAR_OFERTA = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listado_empresas);
         lvEmpresas = findViewById(R.id.lvEmpresas);
+        tvUsuarioTB = findViewById(R.id.tvUsuarioTB); // ToolBar Listado Ofertas
+        btnPublicarOfertaTB = findViewById(R.id.btnPublicarOfertaTB); // ToolBar Listado Ofertas
+
+        user = UserHelper.getUser(this);
+
+        validarBotonesToolBar();
         cargarEmpresas();
         Context ctx = this;
-
         lvEmpresas.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
@@ -43,6 +57,31 @@ public class ListadoEmpresasActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void validarBotonesToolBar(){
+        if(user == null){
+            tvUsuarioTB.setText("");
+            btnPublicarOfertaTB.setText("Iniciar sesión");
+        } else {
+            tvUsuarioTB.setText("Bienvenido: "+ user.getNombre() + " " + user.getNombre());
+            if(user.getIdEmpresa() == null)
+            {
+                btnPublicarOfertaTB.setVisibility(View.GONE);
+            } else {
+                btnPublicarOfertaTB.setText("Publicar oferta");
+            }
+        }
+    }
+
+    public void clickPublicarOfertaOLogin(View v){
+        if(user == null){
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivityForResult(intent, REQUEST_LOGIN);
+        } else {
+            Intent intent = new Intent(this, CrearOfertaActivity.class);
+            startActivityForResult(intent, REQUEST_PUBLICAR_OFERTA);
+        }
     }
 
     private void cargarEmpresas(){
