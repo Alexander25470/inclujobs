f0fb679fbb68a60e53bd3dbd454e7fd612ba5c23
Alexander <alexanderpopp99@gmail.com>
2022-11-06 19:39:58 -0300

    descargar cvs, creo

--- app/src/main/java/com/example/inclujobs/activitys/VerCVsActivity.java
+++ app/src/main/java/com/example/inclujobs/activitys/VerCVsActivity.java
@@ -3,9 +3,14 @@ package com.example.inclujobs.activitys;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.inclujobs.DTOs.CVListadoDTO;
 import com.example.inclujobs.R;
@@ -15,7 +20,12 @@ import com.example.inclujobs.conexion.DataListadoCVs;
 import com.example.inclujobs.conexion.DataListadoOfertas;
 import com.example.inclujobs.entidades.Oferta;
 import com.example.inclujobs.helpers.ICallBack;
+import com.google.gson.Gson;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class VerCVsActivity extends AppCompatActivity {
@@ -28,6 +38,20 @@ public class VerCVsActivity extends AppCompatActivity {
         lvCVs = findViewById(R.id.lvCVs);
         cargarCVs();
 
+        lvCVs.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                CVListadoDTO cv = (CVListadoDTO) lvCVs.getItemAtPosition(i);
+                try {
+                    saveFile(cv.getArchivo(), cv.getNombreArchivo());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Toast toast = Toast.makeText(getApplicationContext(),"Descargando CV: " + cv.getNombreArchivo(), Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
 
     }
 
@@ -38,10 +62,24 @@ public class VerCVsActivity extends AppCompatActivity {
             public void function(Object obj) {
                 listaCVs = (ArrayList<CVListadoDTO>)obj;
                 CVAdapter adapter = new CVAdapter(ctx, listaCVs);
-                //ArrayAdapter<CVListadoDTO> adapter = new ArrayAdapter<CVListadoDTO>(ctx, android.R.layout.simple_list_item_1,listaCVs);
                 lvCVs.setAdapter(adapter);
             }
         });
         task.execute();
     }
+    private void saveFile(byte[] content, String fileName) throws IOException {
+        //File file = new File(Environment.getExternalStorageDirectory() , fileName);
+        File file = new File(getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS) , fileName);
+
+
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            fos.write(content);
+            fos.close();
+        }catch (IOException e){
+            Toast.makeText(this,"Error al descargar CV", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
 }
